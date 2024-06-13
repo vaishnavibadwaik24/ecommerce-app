7664d87e7d50b21fd09021808309d585318e5a93
Vaishnavi A Badwaik <88033256+vaishnavibadwaik24@users.noreply.github.com>
2024-06-13 20:18:35 +0530

    modify contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -29,19 +29,18 @@
                 <div class="col-12">
                     <div class="text-center mx-auto" style="max-width: 700px;">
                         <h1 class="text-primary">Get in touch</h1>
-                        <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
                     </div>
                 </div>
                 <div class="col-lg-12">
                     <div class="h-100 rounded">
                         <iframe class="rounded w-100" 
-                        style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238129.6392450734!2d78.90769237692953!3d21.161326260270194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0a5a31faf13%3A0x19b37d06d0bb3e2b!2sNagpur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1712048939991!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        style="height: 400px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d497698.7749916886!2d77.3012475!3d12.9544595!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1718290009608!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
                 <div class="col-lg-7">
                     <form action="" method="POST">
                         @csrf
-                        <input type="text" name="name" id="name" class="w-100 form-control border-0 py-3 mb-4" placeholder="Your Name">
+                        <input type="text" name="name" id="name" class="w-100 form-control border-0 py-3 mb-4" placeholder="Enter Your Name">
                         <input type="email" name="email" id="email" class="w-100 form-control border-0 py-3 mb-4" placeholder="Enter Your Email">
                         <textarea name="message" id="message" class="w-100 form-control border-0 mb-4" rows="5" cols="10" placeholder="Your Message"></textarea>
                         <button class="w-100 btn form-control border-secondary py-3 bg-white text-primary " type="submit">Submit</button>
@@ -52,21 +51,21 @@
                         <i class="fas fa-map-marker-alt fa-2x text-primary me-4"></i>
                         <div>
                             <h4>Address</h4>
-                            <p class="mb-2">123 Street Nagpur.India</p>
+                            <p class="mb-2">123 Street Bangalore, India</p>
                         </div>
                     </div>
                     <div class="d-flex p-4 rounded mb-4 bg-white">
                         <i class="fas fa-envelope fa-2x text-primary me-4"></i>
                         <div>
                             <h4>Mail Us</h4>
-                            <p class="mb-2">fruitables@gmail.com</p>
+                            <p class="mb-2">deliver@fastfruits.com</p>
                         </div>
                     </div>
                     <div class="d-flex p-4 rounded bg-white">
                         <i class="fa fa-phone-alt fa-2x text-primary me-4"></i>
                         <div>
                             <h4>Telephone</h4>
-                            <p class="mb-2">(+91) 1234 5678 21</p>
+                            <p class="mb-2">(+91) 8309557905</p>
                         </div>
                     </div>
                 </div>
